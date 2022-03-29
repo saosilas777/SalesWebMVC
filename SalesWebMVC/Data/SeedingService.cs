f0ce20701c8abd20bfdfcadac3d9be3f064a13d5
Silas Dias <saosilas@hotmail.com>
2022-03-28 21:18:09 -0300

    replace seedingservice

--- SalesWebMVC/Data/SeedingService.cs
+++ SalesWebMVC/Data/SeedingService.cs
@@ -29,9 +29,9 @@ namespace SalesWebMVC.Data
             Department d4 = new Department(4, "Books");
 
             Seller s1 = new Seller(1, "Bob", "bob@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
-            Seller s2 = new Seller(1, "Maria", "maria@gmail.com", new DateTime(1997, 7, 13), 2000.0, d2);
-            Seller s3 = new Seller(1, "Alex Grey", "alex@gmail.com", new DateTime(1990, 2, 10), 1500.0, d4);
-            Seller s4 = new Seller(1, "Ana", "ana@gmail.com", new DateTime(2000, 5, 11), 900.0, d3);
+            Seller s2 = new Seller(2, "Maria", "maria@gmail.com", new DateTime(1997, 7, 13), 2000.0, d2);
+            Seller s3 = new Seller(3, "Alex Grey", "alex@gmail.com", new DateTime(1990, 2, 10), 1500.0, d4);
+            Seller s4 = new Seller(4, "Ana", "ana@gmail.com", new DateTime(2000, 5, 11), 900.0, d3);
             Seller s5 = new Seller(5, "Donald", "donald@gmail.com", new DateTime(2000, 1, 9), 4000.0, d3);
             Seller s6 = new Seller(6, "Alex Pink", "bob@gmail.com", new DateTime(1997, 3, 4), 3000.0, d2);
 
@@ -67,13 +67,15 @@ namespace SalesWebMVC.Data
             SalesRecord r30 = new SalesRecord(30, new DateTime(2018, 10, 12), 5000.0, SaleStatus.Billed, s2);
 
             _context.Department.AddRange(d1, d2, d3, d4);
+
             _context.Seller.AddRange(s1, s2, s3, s4, s5, s6);
+
             _context.SalesRecord.AddRange(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10,
                 r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
                 r21, r22, r23, r24, r25, r26, r27, r28, r29, r30);
 
-            _context.SaveChanges();
 
+            _context.SaveChanges();
         }
     }
 }
